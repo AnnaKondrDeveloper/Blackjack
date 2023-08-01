16edcc381ee0fd5b3163918f942d2c357ed4960d
Anna Kondrateva <85047120+AnnaKondrDeveloper@users.noreply.github.com>
2023-07-31 23:30:33 -0400

    Created images for cart using API

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
 import './App.css';
+import { useEffect } from 'react';
 
 function App() {
+
+	let cardImage1;
+	let cardImage2;
+
+	useEffect(() => {
+
+	fetch("https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1").then(res => {
+		return res.json();
+	}).then(res => {
+		// eslint-disable-next-line react-hooks/exhaustive-deps
+		let deck_id = res.deck_id;
+		return fetch(`https://deckofcardsapi.com/api/deck/${deck_id}/draw/?count=2`)
+	}).then( res => {
+		return res.json();
+	}).then( res => {
+
+		cardImage1 = res.cards[0].image; 
+		cardImage2 = res.cards[1].image
+		return (cardImage1, cardImage2)
+	})
+  	 }, []);
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+		<div className="card_field">
+			<img src="{cardImage1}" alt="card"></img>
+			<img src="{cardImage2}" alt="card"></img>
+		</div>
+
     </div>
   );
 }
